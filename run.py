ae96d4be500e1967a3e179f2a9a7d8c4f16d8530
worldofmarcus <me@marcuseriksson.com>
2022-08-15 08:02:52 +0000

    Install rich python library and create loop to create rich table from google sheet

--- run.py
+++ run.py
@@ -4,6 +4,9 @@
 
 import gspread
 from google.oauth2.service_account import Credentials
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,4 +22,16 @@ SHEET = GSPREAD_CLIENT.open('music_collection')
 collection = SHEET.worksheet('collection')
 data = collection.get_all_values()
 
-print(data)
+
+
+
+table = Table(title="WoM Record Collection")
+
+for heading in data[0]:
+    table.add_column(f"{heading}")
+
+for row in data[1::1]:
+    table.add_row(*row, style="bold black on white")
+
+console = Console()
+console.print(table)
