161a8b669e184368eae77776a364cebc1ee88ded
worldofmarcus <me@marcuseriksson.com>
2022-08-15 13:15:51 +0000

    Add basic structure for class music

--- run.py
+++ run.py
@@ -22,9 +22,32 @@ SHEET = GSPREAD_CLIENT.open('music_collection')
 collection = SHEET.worksheet('collection')
 data = collection.get_all_values()
 
+class Music():
+    def __init__ (self, catalog, artist, title, label, format, rating, released, sale, notes):
+        # instance attributes
+        self.catalog = catalog
+        self.artist = artist
+        self.title = title
+        self.label = label
+        self.format = format
+        self.rating = rating
+        self.released = released
+        self.sale = sale
+        self.notes = notes
+    
+# create instance
+
+music_item = Music("kfem02", "Det Vita Bruset", "Att Leva I Det Förflutna", "Kollektiv Fem", "Cassette", "4", "2014", "No", "A really nice cassette!")
+print(music_item.label)
 
 
 
+
+
+"""
+Function that loops through the 'music_collection' sheet
+and adds column / rows accordingly
+"""
 table = Table(title="WoM Record Collection")
 
 for heading in data[0]:
@@ -32,6 +55,7 @@ for heading in data[0]:
 
 for row in data[1::1]:
     table.add_row(*row, style="bold black on white")
+
 
 console = Console()
 console.print(table)
